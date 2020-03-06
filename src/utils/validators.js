10d7422bc9e67dac3b0c63085401ab73b658b7ac
Ales Pazniak <a.pazniak@pixelplex.io>
2020-03-06 12:27:26 +0300

    changed validator for tx

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,16 +1,14 @@
 import { validators } from 'echojs-lib';
 import BN from 'bignumber.js';
 
-const isEthereumAddress = (v) => validators.isBytes(v, 21);
-const isEthereumPrefix = (v) => validators.isString(v) && v.slice(0, 2) === '0x';
+const isEthereumAddress = (v) => v.length === 42 && v.slice(0, 2) === '0x' && validators.isBytes(v.slice(2), 20);
 const convertToBN = (v) => new BN(v, 16).toString(10);
 
 export const validateTx = (tx) => {
 	if (!validators.isObject(tx)) throw new Error('transaction should be object');
-	if (tx.to && !isEthereumPrefix(tx.to)) throw new Error('invalid Ethereum prefix, should be "0x"');
-	if (tx.to && !isEthereumAddress(tx.to)) throw new Error('invalid Ethereum address "to"');
+	if ('to' in tx && !isEthereumAddress(tx.to)) throw new Error('invalid Ethereum address "to"');
 	if (!isEthereumAddress(tx.from)) throw new Error('invalid Ethereum address "from"');
-	if (tx.data && !validators.isHex(tx.data)) throw new Error('invalid transaction data');
+	if ('data' in tx && !validators.isHex(tx.data)) throw new Error('invalid transaction "data"');
 	if (!validators.isUInt32(convertToBN(tx.gas))) throw new Error('transaction gas should not be negative');
 	if (!validators.isUInt32(convertToBN(tx.value))) throw new Error('transaction value should not be negative');
 };
