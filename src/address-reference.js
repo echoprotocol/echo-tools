a06ba0c2bb0a8bc8ca08976e470a269c6b8ea2a0
Mikhail Shautsou <mikhail.shautsou@gmail.com>
2020-03-13 12:04:35 +0300

    fix get account reference

--- src/address-reference.js
+++ src/address-reference.js
@@ -1,12 +1,11 @@
 import echoTools from './echo-tools';
-import addressToId from './utils/address-to-id';
 
 import { isEthereumAddress } from './utils/validators';
 
 export default async function getAddressReference(address) {
 	if (!isEthereumAddress(address)) throw new Error('invalid Ethereum address');
 
-	const id = await echoTools.web3.getAddressReference(address);
+	const evmAddress = await echoTools.web3.getAddressReference(address);
 
-	return id ? addressToId(id) : null;
+	return evmAddress;
 }
